958606cf7c222cefa76b707f6099969c1d7874f1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-11-13 19:35:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,7 @@ class Driver{
 let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
 
 class Route{
-  
+  constructor(beginningLocation, endingLocation){
+
+  }
 }
